2b9596764e1108ba301e4827595c9ab3fe134ee4
lisekh <lisehaugen@gmail.com>
2014-04-27 22:44:01 +0200

    added theme menu

--- Magic8Ball/gen/com/example/magic8ball/R.java
+++ Magic8Ball/gen/com/example/magic8ball/R.java
@@ -21,20 +21,27 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int magicball=0x7f020001;
+        public static final int ball3=0x7f020000;
+        public static final int ball4=0x7f020001;
+        public static final int ball5=0x7f020002;
+        public static final int ball6=0x7f020003;
+        public static final int ball7=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int magicball=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
         public static final int magic8ball=0x7f080000;
         public static final int text=0x7f080001;
+        public static final int themeTextView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_magic_main=0x7f030000;
+        public static final int theme_list=0x7f030001;
+        public static final int themetext=0x7f030002;
     }
     public static final class menu {
-        public static final int answer=0x7f070000;
-        public static final int magic_main=0x7f070001;
+        public static final int magic_main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
--- Magic8Ball/src/com/example/magic8ball/MagicMainActivity.java
+++ Magic8Ball/src/com/example/magic8ball/MagicMainActivity.java
@@ -1,14 +1,20 @@
 package com.example.magic8ball;
 
 import java.util.ArrayList;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
 public class MagicMainActivity extends Activity implements SensorEventListener {
@@ -18,24 +24,94 @@ public class MagicMainActivity extends Activity implements SensorEventListener {
 	private TextView textAnswer;
 	private long lastUpdate = 0;
 	private float prevX, prevY, prevZ;
-	private static final int SHAKE_THRESHOLD = 1300;
+	private static final int SHAKE_THRESHOLD = 1200;
 	private Vibrator vib;
-	private long[] pattern = {100, 150, 100};
+	private final String[] THEMES = {"Normal", "Ocean", "Enigma", "Disco", "Fire", "8 ball on fire"};
+	AlertDialog.Builder alert;
+	ImageButton iButton;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) 
+	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_magic_main);
 		
+		// Initialize sensor management
 		sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		accelerometer = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		sManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 		
+		// Initialize imagebitton
+		iButton = (ImageButton) findViewById(R.id.magic8ball);
+		iButton.setOnClickListener(iButtonHandler);
+		
+		// Initialize the answer textfield
 		textAnswer = (TextView) findViewById(R.id.text);
-		textAnswer.setTextSize(40);
+		textAnswer.setTextSize(35);
+		
 		vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 	}
+
+	// Create dialog for changing themes
+	public void showThemes()
+	{
+		AlertDialog.Builder showThemes = new AlertDialog.Builder(this);
+		showThemes.setTitle("Choose theme");
+       
+		// Show possible themes
+		ListAdapter adapter = new ArrayAdapter<String>(this, R.layout.themetext, THEMES);
+	 
+		showThemes.setAdapter(adapter, new OnClickListener() 
+		{
+			public void onClick(DialogInterface dialog, int whichTheme) 
+			{
+				switch(whichTheme) 
+				{
+					case 0:
+						iButton.setBackgroundResource(R.drawable.magicball);
+						break;
+					case 1:
+						iButton.setBackgroundResource(R.drawable.ball3);
+						break;
+					case 2:
+						iButton.setBackgroundResource(R.drawable.ball4);
+						break;
+					case 3:
+						iButton.setBackgroundResource(R.drawable.ball5);
+						break;
+					case 4:
+						iButton.setBackgroundResource(R.drawable.ball6);
+						break;
+					case 5:
+						iButton.setBackgroundResource(R.drawable.ball7);
+						break;
+				}
+			}
+		 });
+		
+		// Show cancel button
+		showThemes.setCancelable(true);
+        showThemes.setNegativeButton("Cancel", new DialogInterface.OnClickListener() 
+        {
+            public void onClick(DialogInterface dialog, int id) 
+            {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = showThemes.create();
+        alertDialog.show();
+	}
+	
+	// Initialize when imagebutton is pressed
+	View.OnClickListener iButtonHandler = new View.OnClickListener() 
+	{
+		@Override
+		public void onClick(View v) 
+		{
+			showThemes();
+		}
+	};
 
 	protected void onPause() 
 	{
@@ -55,6 +131,11 @@ public class MagicMainActivity extends Activity implements SensorEventListener {
 		
 	}
 
+	/**
+	 * Activating the accelerometer threshold
+	 * provides the user with an answer
+	 * @param SensorEvent event
+	 */
 	@Override
 	public void onSensorChanged(SensorEvent event) 
 	{
@@ -69,19 +150,18 @@ public class MagicMainActivity extends Activity implements SensorEventListener {
 			
 			long curTime = System.currentTimeMillis();
 			 
-	        if ((curTime - lastUpdate) > 100) 
+	        if((curTime - lastUpdate) > 150) 
 	        {
 	            long diffTime = (curTime - lastUpdate);
 	            lastUpdate = curTime;
 	            
 	            float speed = Math.abs(x + y + z - prevX - prevY - prevZ)/ diffTime * 10000;
 	            
-	            if (speed > SHAKE_THRESHOLD)
+	            if(speed > SHAKE_THRESHOLD)
 	            {
-	            	vib.vibrate(pattern, -1);
+	            	vib.vibrate(100);
 	            	showAnswer();
 	            }
-	 
 	            prevX = x;
 	            prevY = y;
 	            prevZ = z;
@@ -89,18 +169,21 @@ public class MagicMainActivity extends Activity implements SensorEventListener {
 		}
 	}
 
+	/**
+	 * Generate an answer
+	 */
 	private void showAnswer() 
 	{
 		ArrayList<String> answers = new ArrayList<String>();
-		answers.add("Definitivt");
-		answers.add("Usikkert");
-		answers.add("Selvfølgelig");
-		answers.add("Hva tror du?");
-		answers.add("Eeh... sure");
-		answers.add("Mordi vet");
-		answers.add("Tror nok det ja");
-		answers.add("Tullete spm");
-		
+			answers.add("Definitely");
+			answers.add("Uncertain");
+			answers.add("Of course");
+			answers.add("Retorical question, right?");
+			answers.add("Probably");
+			answers.add("Who knows?!");
+			answers.add("Indeed");
+			answers.add("Don't count on it");
+			
 		int listSize = answers.size();
 	
 		textAnswer.setText(answers.get((int) (Math.random()*listSize)));
